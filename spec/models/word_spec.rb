5d45f15c1ade7836f382b58fb5f37c4cb0e29fbe
Esteban BS <estbs93@gmail.com>
2022-04-19 19:11:08 -0500

    Feat: Resources to Word entity
    Create the view and the controller to Word model

--- spec/models/word_spec.rb
+++ spec/models/word_spec.rb
@@ -4,5 +4,7 @@ RSpec.describe Word, type: :model do
   describe 'columns' do
     it { is_expected.to have_db_column(:value) }
     it { is_expected.to have_db_column(:language) }
+
+    
   end
 end
