17f201040fc76eb3fc6292f4add8ded3f27f788f
Esteban BS <estbs93@gmail.com>
2022-04-22 17:25:34 -0500

    Feat: Use before action into word controller

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,4 +1,6 @@
 class WordsController < ApplicationController
+  before_action :set_word, only: [:show, :edit, :update, :destroy]
+
   def index
     @words = Word.all
   end
@@ -17,15 +19,12 @@ class WordsController < ApplicationController
   end
 
   def show
-    @word = Word.find(params[:id])
   end
 
   def edit
-    @word = Word.find(params[:id])
   end
 
   def update
-    @word = Word.find(params[:id])
     if @word.update(word_params)
       redirect_to word_path(@word)
     else
@@ -34,7 +33,6 @@ class WordsController < ApplicationController
   end
 
   def destroy
-    @word = Word.find(params[:id])
     @word.destroy
     redirect_to words_path
   end
@@ -43,5 +41,9 @@ class WordsController < ApplicationController
 
   def word_params
     params.require(:word).permit(:content, :language_id)
+  end
+
+  def set_word
+    @word = Word.find(params[:id])
   end
 end
