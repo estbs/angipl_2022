a8ee3848c7b90e928eb5245156fc693948dc6b5d
Esteban BS <estbs93@gmail.com>
2022-04-21 19:09:31 -0500

    Refactor: use before and let in test file

--- spec/requests/words_spec.rb
+++ spec/requests/words_spec.rb
@@ -2,23 +2,27 @@ require 'rails_helper'
 
 RSpec.describe WordsController, type: :controller do
   describe 'GET /words' do
+    before { get :index }
+
     context 'When some words present' do
+      let(:word) { create(:word) }
+
       it 'Assigns @words' do
-        word = create(:word)
-        get :index
         expect(assigns(:words)).to eq([word])
+      end
+
+      it 'Renders the index template' do
+        expect(response).to render_template(:index)
       end
     end
 
     context 'When no words' do
       it 'Assigns @words' do
-        get :index
         expect(assigns(:words)).to eq([])
       end
     end
 
     it 'Renders the index template' do
-      get :index
       expect(response).to render_template(:index)
     end
   end
