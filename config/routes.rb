26ca1609a095c8d361ed84b668e77d370f636e35
Esteban BS <estbs93@gmail.com>
2022-04-22 20:47:44 -0500

    Feat: Install Devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+  # Devise
+  devise_scope :user do
+    # Redirests signing out users back to sign-in
+    get 'users', to: 'devise/sessions#new'
+  end
+  devise_for :users
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "words#index"
+  root 'words#index'
 
   resources :words
 end
---
+++ spec/requests/turbo_devise_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+RSpec.describe "TurboDevises", type: :request do
+  describe "GET /index" do
+  end
+end
